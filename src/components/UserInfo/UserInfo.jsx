a8492aae625c556bb1254c9dad37f1808ced085b
Valentyna Chernova <valentyna19960410@gmail.com>
2026-01-14 22:40:03 +0200

    fix solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,8 +1,10 @@
 import './UserInfo.scss';
 
-export const UserInfo = ({ user }) => (
-      <a className="UserInfo" href={`mailto:${user.email}`}>
-        {user.name}
-      </a>
-    );
-
+export const UserInfo = ({ user }) => {
+if (!user) { return null; }
+ return (
+<a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+ )
+}
